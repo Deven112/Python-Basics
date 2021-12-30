bb7f71aa10c7f248c28fe95302a4be3dd8bca1d3
Deven112 <96877594+Deven112@users.noreply.github.com>
2021-12-30 20:22:58 +0530

    Add files via upload

---
+++ Basic of Python.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+#Data Types - 
+
+num1 = 100
+num2 = 200
+num3 = num1+ num2
+print(num3)
+
+
+# In[8]:
+
+
+#Types of data types In pyhton -\
+#1.int 
+#2.float 
+#3.string
+#4.list
+#5.tuple
+#6.dictionary
+#7.set 
+#8.boolean
+
+
+# In[10]:
+
+
+obtained_marks = 15 
+
+print(age)
+
+print(type(age))
+
+
+# In[12]:
+
+
+f = 0.2
+f1 = 5.8
+print(f+f1)
+
+
+# In[ ]:
+
+
+PEP guideline - tells you that how your code should look
+1. Variable name has to be meaningful name 
+2. _ are allowed between two words in variable declaration
+3.We can use number in variable name but not at start
+
+
+# In[14]:
+
+
+s1 = "hello"
+s2 = "python"
+print(s1+s2)
+
+
+# In[15]:
+
+
+l = [1,2,3,4,5, 0.2,0.3, "hello","python"]
+
+
+# In[17]:
+
+
+t = (1,2,3,4,5)
+
+
+# In[18]:
+
+
+d = {"name":"DEV","Age":50,"email":"abc@gmail.com"}
+
+
+# In[19]:
+
+
+s= {1,2,3,4,5,6}
+
+
+# In[ ]:
+
+
+Operators: 
+    1.Aritmatic: +,-,*,/,//,**,%
+    2.Comarision: ==, <,>,<=,>=,!=
+    3.Memership: in ,not in
+    4.Identitiy: is , is not 
+    5.Assignment: +=,-=,*=,/= 
+    6.Logical: and , or 
+    
+
+
+# In[12]:
+
+
+num1 = 500
+num2 =200
+
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+print(num1//num2)
+print(10**2)
+
+print(10%2)
+
+
+# In[15]:
+
+
+num1 = 500
+num2 =500
+
+print(num1==num2)
+
+print(num1>num2)
+
+print(num1<num2)
+
+print(num1!=num2)
+
+
+# In[19]:
+
+
+l=[10,20,30,40,50]
+
+print(20 not in  l)
+
+
+# In[28]:
+
+
+num1 = 300
+num2 = 300
+
+print(num1==num2)
+
+print(num1 is not num2)
+
+
+# In[27]:
+
+
+print(id(num1))
+print(id(num2))
+
+
+# In[29]:
+
+
+num1 =250
+num1+=10
+
+print(num1)
+num1-=20
+print(num1)
+
+
+# In[34]:
+
+
+print(100 == 100 or  100 != 100)
+
